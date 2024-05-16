dafbfb337a69fb6d7b64b7da8529cf419da77ec9
VladoDemcak <VladoDemcak@users.noreply.github.com>
2024-05-16 21:36:54 +0200

    Update dependencies.sh

--- Ubuntu/dependencies.sh
+++ Ubuntu/dependencies.sh
@@ -22,7 +22,7 @@ install_dependency libavformat-dev
 install_dependency libswscale-dev
 install_dependency libdc1394-22-dev
 install_dependency libxine2-dev
-install_dependency libgstreamer0.10-dev
+install_dependency libgstreamer1.0-dev
 install_dependency libgstreamer-plugins-base0.10-dev
 install_dependency libv4l-dev
 install_dependency python-dev
